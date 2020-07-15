14fb71582281e40dec3194438938761b56e33032
naimitsu <68354985+naimitsu@users.noreply.github.com>
2020-07-15 18:28:39 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>NAIMITSU</h1>
+      <h2>privacy matters</h2>
+    </div>
+  );
+}
