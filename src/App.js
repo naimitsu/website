f26c37abe8b3b963225060a05fa334c3758d8ae5
naimitsu <68354985+naimitsu@users.noreply.github.com>
2020-07-15 18:56:10 -0400

    added discord

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
 import "./styles.css";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 export default function App() {
   return (
+    <Router>
+      <div className="App">
+        <h1>NAIMITSU</h1>
+        <h2>privacy matters</h2>
+
+        <Switch>
+          <Route path="/discord">
+            <Discord />
+          </Route>
+          <Route path="/">
+            <App />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Discord() {
+  return (
     <div className="App">
-      <h1>NAIMITSU</h1>
-      <h2>privacy matters</h2>
+      <Link to="/">
+        <h1>NAIMITSU</h1>
+      </Link>
+
+      <br />
+      <br />
+
+      <iframe
+        title="discord-widget"
+        src="https://discord.com/widget?id=733030444962021417&theme=dark"
+        width="350"
+        height="500"
+        allowtransparency="true"
+        frameborder="0"
+      />
     </div>
   );
 }
